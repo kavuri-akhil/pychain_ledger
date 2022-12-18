e2e1e7b1090a75b2218a718928d634b9ff6011fc
Akhil Sai Kavuri <109653396+kavuri-akhil@users.noreply.github.com>
2022-12-18 15:39:44 +1100

    Updated the code

--- pychain.py
+++ pychain.py
@@ -49,9 +49,11 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
-
-
+@dataclass
+class Record: 
+    sender: str
+    receiver: str
+    amount: float
 ################################################################################
 # Step 2:
 # Modify the Existing Block Data Class to Store Record Data
@@ -68,7 +70,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -170,15 +172,15 @@ input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_value = st.text_input("Enter Sender Details")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver_value = st.text_input("Enter Receiver Details")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount_value = st.text_input("Enter the amount")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,6 +191,10 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
+        record = Record,
+        sender = sender_value,
+        receiver = receiver_value,
+        amount = amount_value,
         data=input_data,
         creator_id=42,
         prev_hash=prev_block_hash
